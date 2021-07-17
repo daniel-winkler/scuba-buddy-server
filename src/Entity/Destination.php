a6992dde846245c9afe3ef19bb63bd3d794c4669
dwinkler <danielbwo@gmail.com>
2021-07-17 13:15:05 +0200

    añadimos porpiedad descripcion a nuestros destinos

--- src/Entity/Destination.php
+++ src/Entity/Destination.php
@@ -39,6 +39,11 @@ class Destination
      */
     private $image;
 
+    /**
+     * @ORM\Column(type="string", length=255, nullable=true)
+     */
+    private $description;
+
     public function __construct()
     {
         $this->shops = new ArrayCollection();
@@ -111,6 +116,18 @@ class Destination
     public function setImage(?string $image): self
     {
         $this->image = $image;
+
+        return $this;
+    }
+
+    public function getDescription(): ?string
+    {
+        return $this->description;
+    }
+
+    public function setDescription(?string $description): self
+    {
+        $this->description = $description;
 
         return $this;
     }
