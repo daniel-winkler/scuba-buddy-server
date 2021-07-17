a712e7fdd213c0a19004b608c855a5e975837600
dwinkler <danielbwo@gmail.com>
2021-07-17 12:04:59 +0200

    añadimos propiedad de imagen a los destinos como URL y cargamos el servidor de imagenes

--- src/Entity/Destination.php
+++ src/Entity/Destination.php
@@ -34,6 +34,11 @@ class Destination
      */
     private $shops;
 
+    /**
+     * @ORM\Column(type="string", length=255, nullable=true)
+     */
+    private $image;
+
     public function __construct()
     {
         $this->shops = new ArrayCollection();
@@ -94,6 +99,18 @@ class Destination
                 $shop->setDestination(null);
             }
         }
+
+        return $this;
+    }
+
+    public function getImage(): ?string
+    {
+        return $this->image;
+    }
+
+    public function setImage(?string $image): self
+    {
+        $this->image = $image;
 
         return $this;
     }
