d50941c39815e1832a1437839836735dbd5ce8b2
dwinkler <danielbwo@gmail.com>
2021-07-18 11:18:41 +0200

    creamos el controlador para los envios de emails por el contact form

---
+++ src/Controller/MailerController.php
@@ -0,0 +1,38 @@
+<?php
+
+namespace App\Controller;
+
+use App\Repository\ShopRepository;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Component\Mailer\MailerInterface;
+use Symfony\Component\Mime\Email;
+
+class MailerController extends AbstractController
+{
+    /**
+     * @Route("/mailer", name="mailer", methods={"POST"})
+     */
+    public function sendEmail(MailerInterface $mailer, Request $request, ShopRepository $shopRepository): Response
+    {
+        $messageData = json_decode($request->getContent(), true);
+        $shop = $shopRepository->find($messageData['shopID']);
+        $shopEmail = $shop->getUser()->getEmail();
+        
+        $email = (new Email())
+            ->from($messageData['email'])
+            ->to($shopEmail)
+            ->subject('New message from '.$messageData['name'].' ('.$messageData['email'].')')
+            ->text($messageData['message']);
+
+        $mailer->send($email);
+
+        return $this->json([
+            'ok' => true
+        ],
+            Response::HTTP_ACCEPTED
+        );
+    }
+}
