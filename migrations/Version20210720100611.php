91fe78567ed728abf7daf93e0f80920811d7e05d
dwinkler <danielbwo@gmail.com>
2021-07-20 12:22:05 +0200

    instalamos en windows para testear, modificamos .env y .env.local

--- migrations/Version20210720100611.php
+++ migrations/Version20210720100611.php
@@ -10,7 +10,7 @@ use Doctrine\Migrations\AbstractMigration;
 /**
  * Auto-generated Migration: Please modify to your needs!
  */
-final class Version20210720090147 extends AbstractMigration
+final class Version20210720100611 extends AbstractMigration
 {
     public function getDescription(): string
     {
@@ -24,6 +24,7 @@ final class Version20210720090147 extends AbstractMigration
         $this->addSql('CREATE TABLE picture (id INT AUTO_INCREMENT NOT NULL, shop_id INT NOT NULL, url VARCHAR(255) NOT NULL, INDEX IDX_16DB4F894D16C4DD (shop_id), PRIMARY KEY(id)) DEFAULT CHARACTER SET utf8mb4 COLLATE `utf8mb4_unicode_ci` ENGINE = InnoDB');
         $this->addSql('ALTER TABLE divelog ADD CONSTRAINT FK_C77A4A93A76ED395 FOREIGN KEY (user_id) REFERENCES user (id)');
         $this->addSql('ALTER TABLE picture ADD CONSTRAINT FK_16DB4F894D16C4DD FOREIGN KEY (shop_id) REFERENCES shop (id)');
+        $this->addSql('ALTER TABLE user CHANGE roles roles LONGTEXT NOT NULL COMMENT \'(DC2Type:json)\'');
     }
 
     public function down(Schema $schema): void
@@ -31,5 +32,6 @@ final class Version20210720090147 extends AbstractMigration
         // this down() migration is auto-generated, please modify it to your needs
         $this->addSql('DROP TABLE divelog');
         $this->addSql('DROP TABLE picture');
+        $this->addSql('ALTER TABLE user CHANGE roles roles LONGTEXT CHARACTER SET utf8mb4 NOT NULL COLLATE `utf8mb4_bin`');
     }
 }
