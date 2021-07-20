da066851c4d416e026dcc98b10cd0dcf8140cce6
dwinkler <danielbwo@gmail.com>
2021-07-20 11:06:31 +0200

    vovlemos a generar una migraciones para que no cometa errores

---
+++ migrations/Version20210720090147.php
@@ -0,0 +1,35 @@
+<?php
+
+declare(strict_types=1);
+
+namespace DoctrineMigrations;
+
+use Doctrine\DBAL\Schema\Schema;
+use Doctrine\Migrations\AbstractMigration;
+
+/**
+ * Auto-generated Migration: Please modify to your needs!
+ */
+final class Version20210720090147 extends AbstractMigration
+{
+    public function getDescription(): string
+    {
+        return '';
+    }
+
+    public function up(Schema $schema): void
+    {
+        // this up() migration is auto-generated, please modify it to your needs
+        $this->addSql('CREATE TABLE divelog (id INT AUTO_INCREMENT NOT NULL, user_id INT NOT NULL, date DATE NOT NULL, location VARCHAR(64) NOT NULL, divetype VARCHAR(64) NOT NULL, INDEX IDX_C77A4A93A76ED395 (user_id), PRIMARY KEY(id)) DEFAULT CHARACTER SET utf8mb4 COLLATE `utf8mb4_unicode_ci` ENGINE = InnoDB');
+        $this->addSql('CREATE TABLE picture (id INT AUTO_INCREMENT NOT NULL, shop_id INT NOT NULL, url VARCHAR(255) NOT NULL, INDEX IDX_16DB4F894D16C4DD (shop_id), PRIMARY KEY(id)) DEFAULT CHARACTER SET utf8mb4 COLLATE `utf8mb4_unicode_ci` ENGINE = InnoDB');
+        $this->addSql('ALTER TABLE divelog ADD CONSTRAINT FK_C77A4A93A76ED395 FOREIGN KEY (user_id) REFERENCES user (id)');
+        $this->addSql('ALTER TABLE picture ADD CONSTRAINT FK_16DB4F894D16C4DD FOREIGN KEY (shop_id) REFERENCES shop (id)');
+    }
+
+    public function down(Schema $schema): void
+    {
+        // this down() migration is auto-generated, please modify it to your needs
+        $this->addSql('DROP TABLE divelog');
+        $this->addSql('DROP TABLE picture');
+    }
+}
